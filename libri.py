e89ff00af4b2f6932a429312553483c4e8db0017
ilMazzu <mazzuv3@gmail.com>
2024-11-27 13:09:59 +0100

    primo commit

---
+++ libri.py
@@ -0,0 +1,32 @@
+catalogo = []
+libriprestati = []
+
+def aggiuntalibro():
+    titolo = input("Come si chiama il Libro?")
+    catalogo.append(titolo)
+    print("il tuo libro chiamato", titolo, "è stato aggiunto al catalogo" )
+    print(catalogo)
+
+def prestitolibro(titolo):
+    
+    catalogo.remove(titolo)
+    libriprestati.append(titolo)
+    print("libri prestati: ", libriprestati)
+    print("catalogo: ", catalogo)
+
+def riportalibro(titolo):
+    catalogo.append(titolo)
+    print("Hai riportato il libro chiamato", titolo, ", è stato aggiunto al catalogo" )
+    print(catalogo)
+
+def disponibilelibro(titolo):
+    if titolo in catalogo:
+        print("Lo abbiamo")
+    elif titolo in libriprestati:
+        print("Attualmente non è disponibile, ma tornerà presto")     
+    else:
+        print("Non lo abbiamo mi dispiace")
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,40 @@
+from libri import *
+scelta = 8
+while scelta != 0:
+    print("-------------------------------------------------------------")
+    print("Benvenuti in libreria")
+    print("1 - Aggiungi un Libro")
+    print("2 - Prendi un libro in prestito")
+    print("3 - Riporta un libro")
+    print("4 - Controlla disponbibilità di un Libro")
+    print("5 - Lista dei libri disponibili")
+    print("6 - Libri attualmente non disponibili (in prestito)")
+    print("0 - Niente")
+
+    scelta = int(input("Cosa vorresti fare? "))
+
+    if scelta == 1:
+        aggiuntalibro()
+
+    elif scelta == 2:
+        titolo = input("Che libro vuoi prendere in prestito?") 
+        prestitolibro(titolo)
+    
+    elif scelta == 3:
+        titolo = input("Che libro vuoi riportare?")
+        riportalibro(titolo)
+    
+    elif scelta == 4:
+        titolo = input("Che libro stai cercando?")
+        disponibilelibro(titolo)
+
+    elif scelta == 5:
+        print(catalogo)
+
+    elif scelta == 6:
+        print(libriprestati)
+    elif scelta == 0:
+        print("Grazie di aver utilizzato il nostro gestionale")
+
+
+
